b06bad8a5a737e723651dc26fd9dec714cdf3f11
Raymond Lin <rayster264@gmail.com>
2017-12-07 12:58:18 -0500

    commit 1

--- CYOA/src/game/Game.java
+++ CYOA/src/game/Game.java
@@ -1,7 +1,7 @@
 package game;
 
 import java.awt.*;
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class Game {
 	
@@ -17,6 +17,10 @@ public class Game {
 		window.setSize(800,700);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.getContentPane().setBackground(Color.black);
+		JLabel welcome = new JLabel("CYOA");
+		welcome.setForeground(Color.white);
+		welcome.setBounds(50,100, 100,30);  
+		window.add(welcome);
 		window.setLayout(null);
 		window.setVisible(true);
 	}
