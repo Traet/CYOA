81d0993deb25eb9e425a435a5248f91ee1383cb1
Raymond Lin <rayster264@gmail.com>
2017-12-07 15:43:56 -0500

    fixed something

--- CYOA/src/game/Intro_Selection.java
+++ CYOA/src/game/Intro_Selection.java
@@ -1,8 +1,7 @@
 package game;
 
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+
 public class Intro_Selection {
 
 	public static void main(String[] args) {
