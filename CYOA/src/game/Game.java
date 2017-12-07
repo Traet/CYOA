9fc3d7cd650cb1e8195a26511c273ceb031bad0f
Raymond Lin <rayster264@gmail.com>
2017-12-07 15:52:06 -0500

    commit 2

--- CYOA/src/game/Game.java
+++ CYOA/src/game/Game.java
@@ -17,11 +17,19 @@ public class Game {
 		window.setSize(800,700);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.getContentPane().setBackground(Color.black);
+		
 		JLabel welcome = new JLabel("CYOA");
 		welcome.setFont(new Font("Serif", Font.PLAIN, 30));
 		welcome.setForeground(Color.white);
 		welcome.setBounds(350,100, 500,100);  
-		window.add(welcome);
+		
+		JButton start = new JButton("Start");
+		start.setFont(new Font("Serif", Font.PLAIN, 20));
+		start.setOpaque(true);
+//		start.setForeground(Color.white);
+		start.setBounds(350,400, 100,30);
+		
+		window.add(welcome);window.add(start);
 		window.setLayout(null);
 		window.setVisible(true);
 	}
