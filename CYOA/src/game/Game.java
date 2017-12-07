5a4095ae23ec26051479c39eafa76b33099897f4
Raymond Lin <rayster264@gmail.com>
2017-12-06 20:38:25 -0500

    game window 1

---
+++ CYOA/src/game/Game.java
@@ -0,0 +1,23 @@
+package game;
+
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class Game {
+	
+	JFrame window;
+
+	public static void main(String[] args) {
+		
+		new Game();
+	}
+	
+	public Game() {
+		window = new JFrame();
+		window.setSize(800,700);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.black);
+		window.setLayout(null);
+		window.setVisible(true);
+	}
+}
