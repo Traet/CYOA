2e043ad5365ac69cb412cbb8b4f1fa7cc42b7672
Raymond Lin <rayster264@gmail.com>
2017-12-07 19:44:39 -0500

    title and start button

--- CYOA/src/game/Game.java
+++ CYOA/src/game/Game.java
@@ -6,31 +6,52 @@ import javax.swing.*;
 public class Game {
 	
 	JFrame window;
-
+	Container con;
+	JPanel titlePanel, startPanel;
+	JLabel titleLabel;
+	JButton startButton;
+	Font titleFont = new Font("Times New Roman",Font.PLAIN, 90);
+	Font normalFont = new Font("Times New Roman", Font.PLAIN,28);
+	
 	public static void main(String[] args) {
 		
 		new Game();
 	}
 	
 	public Game() {
-		window = new JFrame();
-		window.setSize(800,700);
+		window = new JFrame("Game");
+		window.setSize(800,600);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.getContentPane().setBackground(Color.black);
-		
-		JLabel welcome = new JLabel("CYOA",SwingConstants.CENTER);
-		welcome.setFont(new Font("Serif", Font.PLAIN, 30));
-		welcome.setForeground(Color.white);
-		welcome.setBounds(350,100, 500,100);  
-		
-		JButton start = new JButton("Start");
-		start.setFont(new Font("Serif", Font.PLAIN, 20));
-		start.setOpaque(true);
-		start.setFocusPainted(false);
-		start.setBounds(350,400, 100,30);
-		
-		window.add(welcome);window.add(start);
 		window.setLayout(null);
 		window.setVisible(true);
+		con = window.getContentPane();
+		
+		
+		titlePanel = new JPanel();
+		titlePanel.setBounds(100,100,600,150); //(x,y, width, heigh) starts at 100,100
+		titlePanel.setBackground(Color.BLACK);
+		titleLabel = new JLabel("UTS CYOA");
+		titleLabel.setBackground(Color.black);
+		titleLabel.setForeground(Color.white);
+		titleLabel.setFont(titleFont);
+		
+		startPanel = new JPanel();
+		startPanel.setBounds(300,400,200,100);
+		startPanel.setBackground(Color.BLACK);
+		
+		startButton = new JButton("START");
+		startButton.setFocusPainted(false);
+		startButton.setBackground(Color.black);
+		startButton.setForeground(Color.white);
+		startButton.setFont(normalFont);
+		
+		
+		titlePanel.add(titleLabel);
+		startPanel.add(startButton);
+		
+		con.add(titlePanel);
+		con.add(startPanel);
+
 	}
 }
