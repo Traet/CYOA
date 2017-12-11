b06bb822955ead4a392188fcaa2daf5a1ab4ea2f
Raymond Lin <rayster264@gmail.com>
2017-12-11 14:14:35 -0500

    character selection improvements

--- CYOA/src/game/Character.java
+++ CYOA/src/game/Character.java
@@ -7,12 +7,18 @@ public class Character {
 		name = n;
 		brain = i;
 	}
+	void display(){System.out.println("Name:"+name" Brains:" + brain);}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Jock a = new Jock("John");
+		a.display();
+		
 	}
 
 }
 class Jock extends Character{
-	
+
+	Jock(String name) {
+		super(name, 1);
+	}
 }
